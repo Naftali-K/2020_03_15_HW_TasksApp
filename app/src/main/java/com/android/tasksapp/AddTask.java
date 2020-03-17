d9f048b50e6c764cf6634c98910050b22b17c6ef
Naftali <naftali@bk.ru>
2020-03-17 23:28:02 +0200

    * Add new class DataManager, static class, for save data * update functionality of AddTask   - Getting input user from forms   - Checking if name task not empty     # if empty, so showing message "Empty name of task"     # if not empty, so moving to next stop   - Made 2 options add + transfer new task for add to TaskList     # streetly in this page we add new object Task to ArrayList in DataManager     # tranfering to other page GSON object, of in other paje already add to ArrayList in DataManager  * Activity_add_task.xml - add "id" to EditText with task note

--- app/src/main/java/com/android/tasksapp/AddTask.java
+++ app/src/main/java/com/android/tasksapp/AddTask.java
@@ -6,7 +6,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AddTask extends AppCompatActivity {
 
@@ -18,11 +22,35 @@ public class AddTask extends AppCompatActivity {
         findViewById(R.id.btn_add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
 
+                TextView taskNameView = findViewById(R.id.task_name);
+                    String taskName = taskNameView.getText().toString();
+                TextView taskNoteView = findViewById(R.id.task_note);
+                    String taskNote = taskNoteView.getText().toString();
 
-                setResult(Activity.RESULT_OK, intent);
-                finish();
+                if(taskName.equals("")){
+                    Toast.makeText(getBaseContext(), "You not wrode name task", Toast.LENGTH_LONG).show();
+                }else{
+
+                    Task task = new Task(taskName, taskNote); //making new task
+                    //option 1
+                    DataManager.addTask(task); //add new task to list of tasks
+
+                    //option 2
+                    JSONObject object = new JSONObject();
+                    try {
+                        object.put("NameTask", taskName);
+                        object.put("note", taskNote);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    Intent intent = new Intent();
+                        intent.putExtra("newTask", object.toString()); //for option 2
+
+                    setResult(Activity.RESULT_OK, intent); //back with result OK
+                    finish(); //close this LayOut
+                }
             }
         });
     }
---
+++ app/src/main/java/com/android/tasksapp/DataManager.java
@@ -0,0 +1,19 @@
+package com.android.tasksapp;
+
+import java.util.ArrayList;
+
+public class DataManager {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+    public static ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public static void setTasks(ArrayList<Task> tasks) {
+        DataManager.tasks = tasks;
+    }
+
+    public static void addTask(Task tast){
+        tasks.add(tast);
+    }
+}
